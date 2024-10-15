4f128039d6ade6b4245533a4cc0d33632c25dfe5
Jonathan Carrier <jonathancarrier@gmail.com>
2024-10-15 13:56:46 +0000

    W4D2

--- app/actions.rb
+++ app/actions.rb
@@ -1,52 +1,35 @@
-def humanized_time_ago(minute_num)
-  if minute_num >= 60
-    "#{minute_num / 60} hours ago"
+get '/' do
+  @finstagram_posts = FinstagramPost.order(created_at: :desc)
+  erb(:index)
+end
+
+get '/signup' do     # if a user navigates to the path "/signup",
+  @user = User.new   # setup empty @user object
+  erb(:signup)       # render "app/views/signup.erb"
+end
+
+post '/signup' do
+  email      = params[:email]
+  avatar_url = params[:avatar_url]
+  username   = params[:username]
+  password   = params[:password]
+
+  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+
+  if @user.save
+    "User #{username} saved!"
   else
-    "#{minute_num} minutes ago"
+    erb(:signup)
   end
 end
 
-get '/' do
-  @finstagram_post_shark = {
-    username: "sharky_j",
-    avatar_url: "https://learningimages.lighthouselabs.ca/flickr-assets/52358606250_01c667c5da_w.jpg",
-    photo_url: "https://learningimages.lighthouselabs.ca/flickr-assets/52358421508_786aa10e2c_c.jpg",
-    humanized_time_ago: humanized_time_ago(15),
-    like_count: 0,
-    comment_count: 1,
-    comments: [{
-      username: "sharky_j",
-      text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
-    }]
-  }
+class User < ActiveRecord::Base
 
-  @finstagram_post_whale = {
-    username: "kirk_whalum",
-    avatar_url: "https://learningimages.lighthouselabs.ca/flickr-assets/52358421348_f34c7996b1.jpg",
-    photo_url: "https://learningimages.lighthouselabs.ca/flickr-assets/52357237337_1cc718f6a7_4k.jpg",
-    humanized_time_ago: humanized_time_ago(65),
-    like_count: 0,
-    comment_count: 1,
-    comments: [{
-      username: "kirk_whalum",
-      text: "#weekendvibes"
-    }]
-  }
+  # ... The rest of your code is still here ...
 
-  @finstagram_post_marlin = {
-    username: "marlin_peppa",
-    avatar_url: "https://learningimages.lighthouselabs.ca/flickr-assets/52358415933_0a0e6bc35f_3k.jpg",
-    photo_url: "https://learningimages.lighthouselabs.ca/flickr-assets/52358494794_f88b160d15_4k.jpg",
-    humanized_time_ago: humanized_time_ago(190),
-    like_count: 0,
-    comment_count: 1,
-    comments: [{
-      username: "marlin_peppa",
-      text: "lunchtime! ;)"
-    }]
-  }
-  @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
-  [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin].to_s
+  # --- Add these lines ---
+  validates :email, :username, uniqueness: true
+  validates :email, :avatar_url, :username, :password, presence: true
+  # -----------------------
 
-  erb(:index)
-  end
+end
